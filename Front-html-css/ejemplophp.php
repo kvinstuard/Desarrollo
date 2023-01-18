13109ab44909a07a1eeceaf046cb6e1fb4a763b5
Kevin Stuard Marin C <kevin.concha@correounivalle.edu.co>
2023-01-18 17:25:01 -0500

    Cambios
    Se agrega un scroll a la tabla con bootstrap, se anexa un archivo de guia de como se deberian agregar los datos desde la BD.

---
+++ Front-html-css/ejemplophp.php
@@ -0,0 +1,23 @@
+<?php
+$conexion = mysqli_connect();
+
+if($conexion){
+    $consulta = 'SELECT * FROM productos';
+    $datos = $conexion -> query($consulta);
+    if($datos->num_rows > 0){
+        while($fila = $datos->fetch_assoc()){
+
+        $nombreProducto = $fila['NOMBRE'];
+        $codigo = $fila['CODIGO'];
+        $total = $fila['TOTAL'];
+?>
+        <tr >
+            <td style="width: 250px"><?= $codigo ?></td>
+            <td style="width: 290px"><?= $nombreProducto ?></td>
+            <td style="width: 241.5px"><?= $total ?></td>
+        </tr>
+<?php
+       }
+  }
+}
+?>
